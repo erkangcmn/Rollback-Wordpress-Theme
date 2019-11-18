b8fae25b83c9e22c4703bf52d5ec6c11eee55d13
erkan <erkangocmen19@gmail.com>
2019-11-18 14:59:33 +0300

    foto_cekme

--- govde.php
+++ govde.php
@@ -20,8 +20,8 @@ if($i > 0 && $i < 5 ){
 ?>
 
   <div class="col-6">
-    <div class="card" style="margin:auto">
-      <img class="card-img-top" width="334" height="167" src="<?php if(has_post_thumbnail()) {the_post_thumbnail();}?>">
+    <div class="card" style="width:21rem;margin:auto">
+      <?php if(has_post_thumbnail()) {the_post_thumbnail(array(334));}?>
       <div class="card-body">
         <h5 class="card-title"><?php the_title(); ?></h5>
         <p class="card-text"><?php the_content(); ?></p>
@@ -40,7 +40,7 @@ if($i >=4 && $i <= 6){
 
   <div class="col-12 mt-3">
       <div class="card" style="margin:auto">
-          <img class="card-img-top" width="710" height="356" src="<?php if(has_post_thumbnail()) {the_post_thumbnail();}?>">
+       <?php if(has_post_thumbnail()) {the_post_thumbnail(array(710));}?>
           <div class="card-body">
             <h5 class="card-title"><?php the_title(); ?></h5>
             <p class="card-text">İlk içerik.</p>
